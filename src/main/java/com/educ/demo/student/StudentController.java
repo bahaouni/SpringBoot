40bae22ea953b07db55ff9106e195fdf6d6bfde9
baha ouni <bahaouni1@gmail.com>
2023-09-30 15:55:19 +0100

    third

--- src/main/java/com/educ/demo/student/StudentController.java
+++ src/main/java/com/educ/demo/student/StudentController.java
@@ -1,24 +1,49 @@
 package com.educ.demo.student;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
-    private StudentService service ;
 
-    public StudentController(StudentService service){
-        this.service = service ;
+    private final StudentService service;
+
+public StudentController(StudentService service){
+    this.service=service;
+}
+    @PostMapping
+    public Student save(
+            @RequestBody Student student
+    ) {
+        return service.save(student);
     }
 
-    @GetMapping("/")
+    @GetMapping("/{email}")
+    public Student findByEmail(
+            @PathVariable("email") String email
+    ) {
+        return service.findByEmail(email);
+    }
+
+    @GetMapping
     public List<Student> findAllStudents() {
-    return  service.findAllStudents();}
+        return service.findAllStudents();
     }
 
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ) {
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(
+            @PathVariable("email") String email
+    ) {
+        service.delete(email);
+    }
+
+}
--- src/main/java/com/educ/demo/student/StudentService.java
+++ src/main/java/com/educ/demo/student/StudentService.java
@@ -1,16 +1,16 @@
+
 package com.educ.demo.student;
 
-import org.springframework.stereotype.Service;
+        import java.util.List;
 
-import java.time.LocalDate;
-import java.util.List;
+public interface StudentService {
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents(){
 
-        return List.of(
-                new Student("baha", "gouni", LocalDate.now(), "bahaouni",22)
-        );
-    }
+
 }
+
